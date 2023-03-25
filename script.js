959649067552cb4ff840fd2f5ccd2cd59e4ea61b
Victoria Sardelli <vlsardelli@outlook.com>
2023-03-25 11:10:08 +0900

    Fixes reset button class name and starts querying elements in js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const bill = document.getElementById("bill");
+const people = document.getElementById("people");
+const tipInput = document.getElementById("tip-input");
+const tipBtns = document.querySelectorAll(".tip-btn");
+const tipPrice = document.getElementById("tip-price");
+const totalPrice = document.getElementById("total-price");
+const resetBtn = document.querySelector(".btn-reset");
